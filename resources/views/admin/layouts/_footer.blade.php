7d7aa902d60e3e4209d0dd19a9cec38c4a078cf4
Ming <mm@bigecko.com>
2018-01-12 17:34:02 +0800

    url

--- resources/views/admin/layouts/_footer.blade.php
+++ resources/views/admin/layouts/_footer.blade.php
@@ -2,5 +2,5 @@
   <div class="pull-right hidden-xs">
     <b>Version</b> 1.0.1
   </div>
-  <strong>Copyright &copy; 2010-2017 <a href="http://bigecko.com">Bigecko</a>.</strong> All rights reserved.
+  <strong>Copyright &copy; 2010-2017 <a href="https://wwww.lanmeng.org">Lanmeng</a>.</strong> All rights reserved.
 </footer>
